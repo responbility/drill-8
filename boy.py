d0545d88a71f1c8ac1327f5c848d9a2a7cb2f346
duruwell <saomaster4@gmail.com>
2025-10-17 23:59:56 +0900

    auto run 마지막 수정

--- boy.py
+++ boy.py
@@ -129,8 +129,8 @@ class Boy:
             {
                 self.SLEEP: {space_down: self.IDLE},
                 self.IDLE:{right_up: self.RUN,left_up: self.RUN, right_down: self.RUN, left_down: self.RUN,time_out: self.SLEEP,a_down: self.AUTO_RUN},
-                self.RUN:{left_down: self.IDLE, right_down: self.IDLE, right_up: self.IDLE, left_up: self.IDLE},
-           self.AUTO_RUN:{time_out: self.IDLE}
+                self.RUN: {left_down: self.IDLE, right_down: self.IDLE, right_up: self.IDLE, left_up: self.IDLE},
+                self.AUTO_RUN: {time_out: self.IDLE,right_down: self.IDLE,right_up: self.IDLE,left_up: self.IDLE},
             }
         )
 
